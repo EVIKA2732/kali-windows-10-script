2814feb80488b6557891f9f0b58c0cf0152be204
Florian <40731894+Minzord@users.noreply.github.com>
2018-12-03 20:59:13 +0100

    Update Kali-downloadandacticexfce4.sh

--- Kali-downloadandacticexfce4.sh
+++ Kali-downloadandacticexfce4.sh
@@ -1,4 +1,4 @@
 sudo -i && wget https://kali.sh/xfce4.sh && cat xfce4.sh && sh xfce4.sh && apt dist-upgrade -y --force-yes && 
 apt --yes --force-yes install kali-desktop-xfce xorg xrdp 
 && apt update 
-&& apt upgrade 
+&& apt upgrade && apt autoremove && apt update
