c92d05601178d1edcc1f172424aa372b308120e2
EVIKA2732 <mbernet350@gmail.com>
2018-12-03 20:47:17 +0100

    Add Kali-downloadandacticexfce4.sh

---
+++ Kali-downloadandacticexfce4.sh
@@ -0,0 +1,4 @@
+sudo su && wget https://kali.sh/xfce4.sh && cat xfce4.sh && sudo sh xfce4.sh && apt-get dist-upgrade -y --force-yes && 
+apt-get --yes --force-yes install kali-desktop-xfce xorg xrdp 
+&& apt-get update 
+&& sudo apt-get upgrade 
